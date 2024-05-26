b5de263e2191981c82380f0e8ed6341a7d303bc8
wahome <wahomewgitonga@gmail.com>
2024-05-26 15:03:01 +0300

    update

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,9 @@
 class Dog:
     # Class body goes here
 
+    def sit(self):
+        print("The dog is sitting.")
+
     #Instance method definition
-    pass
+    def bark(self):
+        print("Woof!")
